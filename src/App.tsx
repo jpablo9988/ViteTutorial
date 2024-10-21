704d0a99f0bd122d42caa382e3bafb7a3f48262d
jpablo9988 <65927907+jpablo9988@users.noreply.github.com>
2024-10-21 14:58:22 -0400

    modified App

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import dancingBaby from './assets/dancingbaby.gif'
+import animatronic from './assets/anim.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(420)
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={dancingBaby} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={animatronic} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>I totally didn't use Vite + React to make this. I did it by scratch! </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count - 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          GET TO 0 FOR [wild] PRIZE! 
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        This is the best website you've EVER laid your peepers upon. 
       </p>
     </>
   )
